530740e2436046bb4f9b0726833957ff03eecbbb
Evgeny Beskonchin <evgeny@beskonchin.info>
2013-03-13 13:18:07 +0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UdxConverter.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var udxFilePath = @"D:\Documents\Phone\CNT.udx";
+            var vcardOutputDirectory = @"D:\Documents\Phone\vcard\";
+
+            var pattern = @"BEGIN:VCARD
+VERSION:2.1
+N;CHARSET=UTF-8;ENCODING=QUOTED-PRINTABLE:{%NAME%}.
+TEL;CELL:{%PHONE%}
+END:VCARD
+";
+            var regex = new Regex("<vCardInfo>.*?</vCardInfo>", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            var nameRegex = new Regex("<N>(.*?)</N>", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            var phoneRegex = new Regex("<TEL>(.*?)</TEL>", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+            var fileContent = File.ReadAllText(udxFilePath);
+
+            foreach(Match data in regex.Matches(fileContent))
+            {
+                var name = nameRegex.Match(data.Value).Groups[1].Value.TrimStart(';');
+                var phone = phoneRegex.Match(data.Value).Groups[1].Value;
+
+                var output = pattern;
+                output = output.Replace("{%NAME%}", name);
+                output = output.Replace("{%PHONE%}", phone);
+
+                File.WriteAllText(Path.Combine(vcardOutputDirectory, data.Index + ".vcf"), output);
+            }
+        }
+    }
+}
